74a92de717e6209cfbb53479d22ae7de2bb2bec5
みる <nomura16623092919@gmail.com>
2026-03-10 23:53:38 +0900

    カメラ

--- app/main.py
+++ app/main.py
@@ -5,5 +5,20 @@ st.title("みーとぅーりすと🍖")
 
 st.subheader("写真を読み込む")
 
+tab1, tab2 = st.tabs(["ファイルから選択", "カメラで撮影"])
 
+with tab1:
+    # ファイル選択
+    upload_file = st.file_uploader("画像を選択してください", type=['png', 'jpg', 'jpeg'])
+    if upload_file:
+        img = Image.open(upload_file)
+        st.image(img, caption="アップロードされた画像", use_container_width=True)
 
+with tab2:
+    # カメラ起動
+    camera_file = st.camera_input("カメラでメモを撮ってください")
+    if camera_file:
+        img = Image.open(camera_file)
+        st.image(img, caption="撮影された画像", use_container_width=True)
+
+target_image = upload_file or camera_file
